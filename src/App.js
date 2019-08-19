54653aed90687706ed0aa35d482ce7a2360be25b
Armando Ruiz <armando.ruiz@industrie.co>
2019-08-19 14:05:23 +1000

    Agregando superhero de bootswatch

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app container">
+      <div className="jumbotron">
+        <p className="lead text-center">Buscador de Imagenes</p>
+
+      </div>
+      <div className="row justify-content-center">
+        
+      </div>
     </div>
   );
 }
