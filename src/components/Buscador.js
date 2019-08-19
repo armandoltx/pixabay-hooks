e79e84a6059dee93eca486e40c105f87ec34bc35
Armando Ruiz <armando.ruiz@industrie.co>
2019-08-19 15:10:16 +1000

    Mostrando la validacion

--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import Error from './Error';
 
 const Buscador = () => {
   // state para hacer la busqueda de las fotos en la API
@@ -23,27 +24,26 @@ const Buscador = () => {
 
 
   return (
-    <div>
-      <form onSubmit={buscarImagen}>
-        <div className="row">
-          <div className="form-group col-md-8">
-            <input
-              type="text"
-              className="form-control form-control-lg"
-              placeholder="Busca una Imagen, ejemplo Cafe"
-              onChange={(e) => (guardarTerminobusqueda(e.target.value))}
-            />
-          </div>
-          <div className="form-group col-md-4">
-            <input
-              type="submit"
-              className="btn btn-lg btn-danger btn-block"
-              value="Buscar"
-            />
-          </div>
+    <form onSubmit={buscarImagen}>
+      <div className="row">
+        <div className="form-group col-md-8">
+          <input
+            type="text"
+            className="form-control form-control-lg"
+            placeholder="Busca una Imagen, ejemplo Cafe"
+            onChange={(e) => (guardarTerminobusqueda(e.target.value))}
+          />
         </div>
-      </form>
-    </div>
+        <div className="form-group col-md-4">
+          <input
+            type="submit"
+            className="btn btn-lg btn-danger btn-block"
+            value="Buscar"
+          />
+        </div>
+      </div>
+      {(error) ? <Error mensaje="Agrega un termino de Busqueda" /> : null}
+    </form>
   );
 };
 
